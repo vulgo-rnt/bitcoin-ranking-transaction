99a91a4707a80239055b9142eb10fd79acd9006f
Renan.T <rtoniolo4@gmail.com>
2023-11-12 17:32:57 -0300

    fix: web socket does not reply

--- service_worker.js
+++ service_worker.js
@@ -1,17 +1,42 @@
 chrome.runtime.onMessage.addListener(({ txid, confirmations }) => {
-  createWs(txid, confirmations);
+  checkConfirmations(txid, confirmations);
 });
 
-async function createWs(txid, confirmations) {
+function checkConfirmations(txid, confirmations) {
+  fetch(`https://api.blockcypher.com/v1/btc/main/txs/${txid}`)
+    .then((data) => data.json())
+    .then((data) => {
+      if (data.confirmations >= confirmations) {
+        console.log("BTC TX SUCESSFULLY");
+      } else {
+        createWs(txid, confirmations);
+      }
+    });
+}
+
+function createWs(txid, confirmations) {
   const ws = new WebSocket(
-    `wss://socket.blockcypher.com/v1/btc/main?token=0a05df34698542dbba64d037419d3a5b/tx/${txid}`
+    `wss://socket.blockcypher.com/v1/btc/main?token=0a05df34698542dbba64d037419d3a5b`
   );
 
   ws.onmessage = (event) => {
     const tx = JSON.parse(event.data);
     console.log(tx);
   };
-  ws.onopen = (event) => {
-    ws.send(JSON.stringify({ event: "tx-confirmation" }));
+  ws.onopen = () => {
+    ws.send(
+      JSON.stringify({
+        event: "tx-confirmation",
+        confirmations: Number(confirmations),
+        hash: txid,
+      })
+    );
+  };
+  ws.onerror = (error) => {
+    console.error("Error connection WebSocket:", error);
+  };
+
+  ws.onclose = (event) => {
+    console.log("Connection WebSocket closed:", event.code, event.reason);
   };
 }
