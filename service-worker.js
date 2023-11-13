6de99f09f76582becbe69672d461b66c790d239b
Renan.T <rtoniolo4@gmail.com>
2023-11-13 15:41:47 -0300

    adjust: request repetition

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,34 @@
-let ws;
+let previousTxId;
 
 chrome.runtime.onMessage.addListener(({ txid, confirmations }) => {
   checkConfirmations(txid, confirmations);
 });
 
 function checkConfirmations(txid, confirmations) {
+  if (previousTxId === txid) return;
+
   fetch(`https://api.blockcypher.com/v1/btc/main/txs/${txid}`)
     .then((data) => data.json())
     .then((data) => {
       if (data.confirmations >= confirmations) {
-        sendMessageNotification(data.confirmations);
+        sendMessageNotification(data);
       } else {
         createWs(txid, confirmations);
       }
+      previousTxId = txid;
     });
 }
 
 function createWs(txid, confirmations) {
-  if (ws?.readyState === WebSocket.OPEN) return;
+  if (txid === previousTxId) return;
 
-  ws = new WebSocket(
+  const ws = new WebSocket(
     `wss://socket.blockcypher.com/v1/btc/main?token=0a05df34698542dbba64d037419d3a5b`
   );
 
   ws.onmessage = (event) => {
     const tx = JSON.parse(event.data);
-    sendMessageNotification(tx.confirmations);
+    sendMessageNotification(tx);
     ws.close();
   };
   ws.onopen = () => {
@@ -46,9 +49,11 @@ function createWs(txid, confirmations) {
   };
 }
 
-function sendMessageNotification(confimations) {
+function sendMessageNotification(tx) {
   const options = {
-    body: `Confirmations : ${confimations}`,
+    body: `
+    TxId : ${tx.hash}
+    Confirmations : ${tx.confirmations}`,
   };
   self.registration.showNotification("BTC TX SUCESSFULLY", options);
 }
